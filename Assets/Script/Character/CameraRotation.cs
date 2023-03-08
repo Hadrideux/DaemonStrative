6ee0c666c878e76f9ac6786b9ca4af05f5a2c8e4
WildBlop <98619098+WildBlop@users.noreply.github.com>
2023-03-08 16:19:57 +0100

    Cam

--- Assets/Script/Character/CameraRotation.cs
+++ Assets/Script/Character/CameraRotation.cs
@@ -8,30 +8,19 @@ public class CameraRotation : MonoBehaviour
     [SerializeField] private Transform _target = null;
 
     [SerializeField] private Vector3 _offSet = Vector3.zero;
+    [SerializeField] private float _angle = 90f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _offSet = transform.position - _target.transform.position;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        CameraAngle();
+       
         transform.LookAt(_target.transform.position);
     }
 
-    private void CameraAngle()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            transform.RotateAround(_target.transform.position, Vector3.up, 90f);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.RotateAround(_target.transform.position, Vector3.up, -90f);
-        }
-        transform.position = _target.transform.position + _offSet;
-    }
 }
