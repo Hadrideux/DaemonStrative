487fb0ae686e545add0423f8446fc3b82eea26e4
Hadrien <101469529+Hadrideux@users.noreply.github.com>
2023-03-09 11:08:44 +0100

    [Inventory]
    WIP

--- Assets/Script/DataBase/InventoryManager.cs
+++ Assets/Script/DataBase/InventoryManager.cs
@@ -8,7 +8,7 @@ public class InventoryManager : Singleton<InventoryManager>
     // Start is called before the first frame update
     void Start()
     {
-        ItemData itemAmount = 
+        //ItemData itemAmount = 
     }
 
     // Update is called once per frame
