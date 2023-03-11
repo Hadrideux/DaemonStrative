693984d4f0697969b4c4b09d767dbd2a288f9867
WildBlop <desbrosses.b@gmail.com>
2023-03-11 13:31:18 +0100

    NPC Move

--- Assets/Script/PNJ/MovementNPC.cs
+++ Assets/Script/PNJ/MovementNPC.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 using static UnityEditor.PlayerSettings;
 using static UnityEngine.GraphicsBuffer;
 
 public class MovementNPC : MonoBehaviour
 {
-    
+    [SerializeField] private NavMeshAgent agent;
     [SerializeField] private float _waitTime = 0;
     private int _activePoint = 0;
+    private float _timePatrol = 0;
 
     public WayPoint[] _wayPoints;
 
@@ -31,13 +33,34 @@ public class MovementNPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WalkRound();
     }
 
     private void WalkRound()
     {
+        
         WayPoint actualPoint = _wayPoints[_activePoint];
-        WayPoint nextPoint = _wayPoints[_activePoint + 1];
-        Vector3.MoveTowards(actualPoint.target.transform.position, nextPoint.target.transform.position, 0);
+        _timePatrol += Time.deltaTime;
+
+        if(actualPoint != null && _waitTime > _timePatrol) 
+        {
+
+            agent.SetDestination(actualPoint.target.transform.position);
+           
+        }
+        else if (_waitTime < _timePatrol && _activePoint < _wayPoints.Length-1)
+        {
+            _activePoint += 1;
+            _timePatrol = 0;
+            WalkRound();
+
+            Debug.Log(_activePoint.ToString());
+        }
+        else
+        {
+            _activePoint= 0;
+            WalkRound();
+        }
     }
+       
 }
