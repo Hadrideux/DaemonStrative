63546de1b44d96f985c6adab5f6107a0712eeb0d
WildBlop <98619098+WildBlop@users.noreply.github.com>
2023-03-13 13:32:22 +0100

    pnj

--- Assets/Script/PNJ/PNJController.cs
+++ Assets/Script/PNJ/PNJController.cs
@@ -29,7 +29,6 @@ public class PNJController : MonoBehaviour
     {
 
     }
-    /*
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
@@ -39,7 +38,6 @@ public class PNJController : MonoBehaviour
             CharacterManager.Instance.IsHostile = true;
         }            
     }
-    */
     private void OnTriggerExit(Collider other)
     {
         //PNJManager.Instance.UInteract(false);
