219f1be89e022f8608248fd072431c4be432bf4f
WildBlop <desbrosses.b@gmail.com>
2023-03-22 00:32:22 +0100

    Switch_Scene

--- Assets/Script/SceneLoader/Switch_Button.cs
+++ Assets/Script/SceneLoader/Switch_Button.cs
@@ -1,33 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
-public class TriggerLoadScene : MonoBehaviour
+public class Switch_Button : MonoBehaviour
 {
     [SerializeField] private bool _isGoWitchScene = false;
     [SerializeField] private bool _isGoGameScene = false;
     [SerializeField] private GameObject _blackFade = null;
     [SerializeField] private Animator _blackFadeAnimation = null;
     [SerializeField] private AnimationClip _fadeIn = null;
-    [SerializeField] private RectTransform _activation = null;
-    [SerializeField] private GameObject _VFXPortal = null;
-
     
-    private void OnTriggerEnter(Collider other)
+
+    // Update is called once per frame
+    void Update()
     {
-        if (other.CompareTag("Player"))
-        {
-            _activation.gameObject.SetActive(true);
-            _VFXPortal.SetActive(true);
-        }
+        
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        _activation.gameObject.SetActive(false);
-        _VFXPortal.SetActive(false);
-    }
     public void SwitchScene()
     {
         if (_isGoWitchScene)
@@ -42,4 +31,3 @@ public class TriggerLoadScene : MonoBehaviour
         }
     }
 }
-
